1395f2e5e406590ede4e48eadfcf0e2aaaad9a62
louis.adjambri <louis.adjambri@lynxspa.com>
2020-03-04 00:03:36 +0100

    [GREEN]: passed for onlyNUmberTest

--- src/main/java/lab2/StringCalculator.java
+++ src/main/java/lab2/StringCalculator.java
@@ -1,8 +1,14 @@
 package lab2;
 
+import static java.util.Objects.isNull;
+
 public class StringCalculator implements Calculator {
     @Override
     public int add(String expression) {
-        return 0;
+        if(isNull(expression) || expression.trim().length() == 0) return 0;
+        String[] numbers = expression.split(",");
+        int result = 0;
+        for(String num: numbers) result += Integer.parseInt(num);
+        return result;
     }
 }
