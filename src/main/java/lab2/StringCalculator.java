47f2a3d71f98663d5c84d478e0a05f27a107ae1d
louis.adjambri <louis.adjambri@lynxspa.com>
2020-03-04 00:17:38 +0100

    [GREEN]: passed test input with '\n' as separator

--- src/main/java/lab2/StringCalculator.java
+++ src/main/java/lab2/StringCalculator.java
@@ -6,7 +6,7 @@ public class StringCalculator implements Calculator {
     @Override
     public int add(String expression) {
         if(isNull(expression) || expression.trim().length() == 0) return 0;
-        String[] numbers = expression.split(",");
+        String[] numbers = expression.split(",|\n");
         int result = 0;
         for(String num: numbers) result += Integer.parseInt(num);
         return result;
